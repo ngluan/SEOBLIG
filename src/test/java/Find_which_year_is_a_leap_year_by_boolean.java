8195659f74bc25e1476236dd68c33c6350e9cd01
Luan <globe_909@hotmail.com>
2022-10-25 16:46:38 +0200

    Updated ,refractor

--- src/test/java/Find_which_year_is_a_leap_year_by_boolean.java
+++ src/test/java/Find_which_year_is_a_leap_year_by_boolean.java
@@ -7,7 +7,7 @@ public class Find_which_year_is_a_leap_year_by_boolean {
 
     @ParameterizedTest
     @ValueSource(ints={2000,2024})
-    public void check_leapyears_if_years_is_dividable_with_4_but_not_100_and_dividable_with_400(int years){
+    public void Test_leapyears_if_years_is_dividable_with_4_but_not_100_and_dividable_with_400(int years){
         LeapYear Test = new LeapYear();
         boolean checkLeapYear = Test.isLeapYear(years);
         assertTrue(checkLeapYear);
@@ -15,7 +15,7 @@ public class Find_which_year_is_a_leap_year_by_boolean {
 
     @ParameterizedTest
     @ValueSource(ints={1700,1800,1900,2100})
-    public void check_not_leapyears_if_years_is_not_dividable_with_4_but_dividable_with_100_and_not_400(int years){
+    public void Test_not_leapyears_if_years_is_not_dividable_with_4_but_dividable_with_100_and_not_400(int years){
         LeapYear Test = new LeapYear();
         boolean checkNotLeapYear = Test.isLeapYear(years);
         assertFalse(checkNotLeapYear);
